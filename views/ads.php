2cb20f1e0bb1ae1a139bbdc3180cd81f0372d8d6
Amir Zaidi <azaidi@live.nl>
2016-01-23 21:43:37 +0100

    Added content, renamed router

---
+++ views/ads.php
@@ -0,0 +1,187 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/style/main.css">
+<title>countriesstandoff.com | The site where countries win democratically!</title>
+<meta name="description" content="countriesstandoff.com is a website where people can vote on their/or other countries during a standoff. Every six months the voting resets and new countries will be picked. The country that wins, will be honoured in a special rankinglist that keeps forever!" />
+
+<script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">
+
+	  $(window).load(function() {          
+	  var i =1;
+	  var images = ['/images/bg0.jpg','/images/bg1.jpg'];
+	  var image = $('header');
+
+	                //Initial Background image setup
+	  image.css('background-image', 'url(/images/bg0.jpg)');
+	                //Change image at regular intervals
+
+	  setInterval(function(){  
+	   image.fadeOut(600, function () {
+	   image.css('background-image', 'url(' + images [i++] +')');
+	   image.fadeIn(600);
+	   });
+	   if(i == images.length)
+	    i = 0;
+	  }, 5000);           
+	 });
+
+	</script>
+
+	<script type="text/javascript">
+
+
+
+	</script>
+
+</head>
+
+<body>
+
+<nav>
+
+	<div class="title">
+
+		<a href="../index.php">cso.com</a>
+
+	</div>
+
+	<div class="navcon">
+
+		<ul>
+
+			<li><a href="/index">Home</a></li><li>
+			<a href="/ads">Advertise?</a></li><li>
+			<a href="../about/aboutus.php">About us</a></li><li>
+			<a href="../contact/contactus.php">Contact us</a></li>
+
+		</ul>
+
+	</div>
+
+</nav>
+
+<div class="header">
+
+	<div class="imgheadcon">
+
+		<div class="webstar">
+
+			<h2> Advertise on cso.com </h2>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="bodycontent">
+
+<div class="greyrow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description" id="opg">
+
+				<h3> Are you an advertiser? </h3>
+
+				<div class="par">
+
+					<p> And are you willing to advertise on our website? Then you are on the right page! We are always searching for new advertisers that would like to advertise on our website. We have a wide viewing population throughout the world. 
+						This website is basically an internet ''game''. Well actually better described as an international voting system where you can vote for the country you think is better. This site is time consuming since you are always wondering who will win.</p>
+
+				</div>
+
+				<h3> What kind of ads are we searching for? </h3>
+
+				<div class="par">
+
+					<p> We first would like to negotiate about the type of ad you would like to show on cso.com. This way we can sure understand both sides better and make a good deal. We do have a few rules about the containments of the ad.
+						We do not accept any NSFW, gore, sexual, or any sort of 18+ ad. We would like to keep our website clean from sexual interference with our viewers. Please do not come over with these sort of ads. We will not accept the offer.</p> </br> <p> On the other hand, 
+						we accept ads that are or text or banner form. If you have a video ad and still would like to advertise on cso.com, be sure to contact us about it. There is room for negiotiation about video ads.</p>
+
+				</div>
+
+				<h3> Ads placement on cso.com </h3>
+
+				<div class="par">
+
+					<p> Since we accept different forms of ads, we also offer you places for the ads based on their form. Banner ads will be placed between the grey/white boxes that contain information. See the example below. If you see a row like this with ''Advertise here!'', 
+						there is a big chance we are still searching for an advertiser who can put his banner form ad there. (Example below is not available!). </p> </br>
+
+					<p> However, if you only have text-based ads, then you still are on the right address! We also offer places for text ads. You can put your text ad in the header (under ''Must see also:''). Or there is space on the bottom of the page where you can find ''Privacy policy'' etc.
+						Please contact us about the form of ad, how many you would like to have on cso.com, pricing, type of ad (PPC, CPM) etc.</p>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="whiterow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<p><a href="adverts/ads.html">Advertise Here! (example)</a></p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="greyrow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description" id="opg">
+
+				<h3> Send us an email about it! </h3>
+
+				<div class="par">
+
+					<p>If you are interested in doing business, please send us a mail! Fill in the fields below!</p></br>
+
+					<form action="ads.php" method="post">
+
+					<p> Your email: </p><input type="text"></br>
+
+					<p> Subject: </p><input type="text"></br>
+
+					<p> Containment of email: </p><input type="text" style="width: 60%; height: 30%;"></br>
+
+					<p> Appendix (optional): </p><input type="text"></br>
+
+					</form>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
---
+++ views/index.php
@@ -0,0 +1,683 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/style/main.css">
+<title>countriesstandoff.com | The site where countries win democratically!</title>
+<meta name="description" content="countriesstandoff.com is a website where people can vote on their/or other countries during a standoff. Every six months the voting resets and new countries will be picked. The country that wins, will be honoured in a special rankinglist that keeps forever!" />
+
+<script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+
+	<script type="text/javascript">
+
+	  $(window).load(function() {          
+	  var i =1;
+	  var images = ['/images/bg0.jpg','/images/bg1.jpg'];
+	  var image = $('header');
+
+	                //Initial Background image setup
+	  image.css('background-image', 'url(/images/bg0.jpg)');
+	                //Change image at regular intervals
+
+	  setInterval(function(){  
+	   image.fadeOut(600, function () {
+	   image.css('background-image', 'url(' + images [i++] +')');
+	   image.fadeIn(600);
+	   });
+	   if(i == images.length)
+	    i = 0;
+	  }, 5000);           
+	 });
+
+	</script>
+
+	<script type="text/javascript">
+
+
+
+	</script>
+
+</head>
+
+<body>
+
+<nav>
+
+	<div class="title">
+
+		<a href="index.php">cso.com</a>
+
+	</div>
+
+	<div class="navcon">
+
+		<ul>
+
+			<li><a href="index.php">Home</a></li><li>
+			<a href="#vote">Vote!</a></li><li>
+			<a href="#leaderboard">Leaderboard</a></li><li>
+			<a href="/ads">Advertise?</a></li><li>
+			<a href="#about">About us</a></li><li>
+			<a href="contact/contact.php">Contact us</a></li>
+
+		</ul>
+
+	</div>
+
+</nav>
+
+<header>
+
+	<div class="stylelayer">
+
+		<div class="headcon" id="headerfixedcon">
+
+		<div class="webstar">
+
+			<h2> <a href="https://www.countriesstandoff.com">www.countriesstandoff.com</a> </br></h2>
+
+		</div>
+
+		<div class="webstarinfo">
+
+		<p style="color: red;">China vs India. Two very big countries. They attract people, entrepreneurs, businesses, and all other related stuff. But, no one has voted about the ultimate form of a BRIC country. Here, we vote on the two biggest BRIC countries. These two countries will go head to head in a voting! Who will win? </br><strong>VOTE BELOW!</strong> (Voting ends at 02-02-2016)</p> </br>
+		<p style="font-weight: bold; font-variant: small-caps;"> Must also see: </p>
+		<a href="adverts/ads.html">Advertise Here!</a>
+
+		</div>
+
+		</div>
+
+	</div>
+
+</header>
+
+<div class="bodycontent">
+
+<div class="greyrow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<p><a href="adverts/ads.html">Advertise Here!</a></p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<a id="leaderboard"><div class="whiterow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<h3>Leaderboard</h3>
+				<p>Here you will see the results of every voting contest! They will be put in archive at the end of the year</p>
+
+				<div class="contentrow">
+
+					<div class="col-md-4">
+
+						<div class="contestinfo">
+
+							<h1>India <img src="images/vs.png"/> U.S. America</h1>
+								<div class="datecontest">
+									<p>20-12-2015 to 4-1-2016</p>
+								</div>
+
+								<div class="votingwin">
+									<p><img src="images/winvoting.png"/>India</p>
+								</div>
+
+						</div>
+
+						<div class="contestinfo">
+
+							<h1>China <img src="images/vs.png"/> India</h1>
+								<div class="datecontest">
+									<p id="timer">13d 23:59:34 till end</p>
+								</div>
+
+								<div class="votingwin">
+									<p><img src="images/winvoting.png"/></p>
+								</div>
+
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+						
+						<div class="contestinfo">
+
+							<h1>India <img src="images/vs.png"/> U.S. America</h1>
+								<div class="datecontest">
+									<p>20-12-2015 to 4-1-2016</p>
+								</div>
+
+								<div class="votingwin">
+									<p><img src="images/winvoting.png"/>India</p>
+								</div>
+
+						</div>
+
+						<div class="contestinfo">
+
+							<h1>China <img src="images/vs.png"/> India</h1>
+								<div class="datecontest">
+									<p id="timer">13d 23:59:34 till end</p>
+								</div>
+
+								<div class="votingwin">
+									<p><img src="images/winvoting.png"/></p>
+								</div>
+
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="contestinfo">
+
+							<h1>India <img src="images/vs.png"/> U.S. America</h1>
+								<div class="datecontest">
+									<p>20-12-2015 to 4-1-2016</p>
+								</div>
+
+								<div class="votingwin">
+									<p><img src="images/winvoting.png"/>India</p>
+								</div>
+
+						</div>
+
+						<div class="contestinfo">
+
+							<h1>China <img src="images/vs.png"/> India</h1>
+								<div class="datecontest">
+									<p id="timer">13d 23:59:34 till end</p>
+								</div>
+
+								<div class="votingwin">
+									<p><img src="images/winvoting.png"/></p>
+								</div>
+
+						</div>
+
+					</div>
+
+					<div class="col-md-12">
+
+						<h3> Caption </h3>
+
+						<div class="contentrow">
+
+							<div class="col-md-24">
+								<h1>Versus</h1><img src="images/vs.png"></img><p>The Standoff</p>
+							</div>
+
+							<div class="col-md-24">
+								<h1>Medal</h1><img src="images/winvoting.png"></img><p>Won a voting contest</p>
+							</div>
+
+							<div class="col-md-24">
+								<h1>Trophy </br> Rookie </h1><img src="images/trophy.png"></img><p>Won a year of voting</p>
+							</div>
+
+							<div class="col-md-24">
+								<h1>Honour </br> Veteran </h1><img src="images/honour.png"></img><p>Won two years of voting</p>
+							</div>
+
+							<div class="col-md-24">
+								<h1>Crown </br> Emperor of cso.com</h1><img src="images/win4years.png"></img><p>Won four years of voting</p>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="specialrow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description" id="specialdesc">
+
+				<h5>Explanation</h5>
+				<p id="explp">We will explain you what this site is and what we do on this site</p>
+
+				<div class="contentrow">
+			
+					<div class="col-md-3" id="col-md-i">
+
+						<h4 id="explt"> How to play </h4>
+						<p>An easier game doesn't exists! The name of the website reveals the game. countriesstandoff.com is a game where people can vote for the country they think should win. You can base this on the countries's recent performence, or just because you like that country! Vote below!</p>
+
+						<h4 id="explt"> Entertainment </h4>
+						<p>This game is so easy and is pretty fun to play. You support your countries's name and fame to be the winner. Vote! The winning country will get the price of being honoured forever on our rankinglist! When the timer resets, new countries will standoff against each other! 
+
+					</div>
+
+					<div class="col-md-6" id="col-md-i">
+
+						
+
+					</div>
+
+					<div class="col-md-3" id="col-md-i">
+
+						<h4 id="explt"> Worldwide game </h4>
+						<p>This game is based on worldwide viewers. The more votes, the bigger the conquest. Be prepared to demolish waves of people voting. All that matters, is that your chosen country recieves the honourable price! Be prepared to go ham!</p>
+
+						<h4 id="explt"> Honourable ranking! </h4>
+						<p>We have said it already a lot, but the price to be in a worldwide ranking is fun. Imagine your country to be #1 every contest. It will keep up in the ranking and shows the world that no one can overthrow it. Every year, the country with the most wins, will be honoured throughout the website!</p>
+
+					</div>		
+
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="greyrow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<p><a href="adverts/ads.html">Advertise Here!</a></p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="whiterow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+			<h3> Why vote for China? </h3>
+			<p> We have listed some facts about why you should vote for China! </p>
+
+			</div>
+
+			<div class="contentrow">
+
+				<div class="ex1" id="exfixed">
+
+					<img src="images/ex1.jpg"/>
+
+					<div class="imgtitle">
+
+						<h4> Civilization </h4>
+
+					</div>
+				
+					<div class="imgtext">
+
+						<p>China is often considered the longest continuous civilization, with some historians marking 6000 B.C. as the dawn of Chinese civilization. It also has the world’s longest continuously used written language</p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+				<div class="ex2" id="exfixed">
+
+					<img src="images/ex2.jpg"/>
+
+					<div class="imgtitle">
+
+						<h4> Inventions </h4>
+
+					</div>
+
+					<div class="imgtext">
+
+						<p>The Chinese invented paper, the compass, gunpowder, and printing. Toilet paper was also invented in China in the late 1300s. It was for emperors only.</p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+	<div class="clearfix"></div>
+
+				<div class="ex3" id="exfixed">
+
+					<img src="images/ex3.png"/>
+
+					<div class="imgtitle">
+
+						<h4> B.C. Tech </h4>
+
+					</div>
+
+					<div class="imgtext">
+
+						<p>By the fourth century B.C., the Chinese were drilling for natural gas and using it as a heat source, preceding Western natural gas drilling by about 2,300 years.</p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+				<div class="ex4" id="exfixed">
+
+					<img src="images/ex4.jpg"/>
+
+					<div class="imgtitle">
+
+						<h4> Weaponry </h4>
+
+					</div>
+
+					<div class="imgtext">
+
+						<p>The crossbow was invented and first used by the Chinese. They were also the first in the world to use chemical and gas weapons, 2,000 years before gas was used in Europe during WWI.</p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="greyrow" id="specialad">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<p><a href="adverts/ads.html">Advertise Here!</a></p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="greyrow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+			<h3> Why vote for India? </h3>
+			<p> We have listed some facts about why you should vote for India! </p>
+
+			</div>
+
+			<div class="contentrow">
+
+				<div class="ex1" id="exfixed">
+
+					<img src="images/ex1i.jpg"/>
+
+					<div class="imgtitle">
+
+						<h4> Civilization </h4>
+
+					</div>
+				
+					<div class="imgtext">
+
+						<p>The Indus valley civilization was one of the most advance civilizations in terms of town planning etc. </p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+				<div class="ex2" id="exfixed">
+
+					<img src="images/ex2i.jpg"/>
+
+					<div class="imgtitle">
+
+						<h4> Maths </h4>
+
+					</div>
+
+					<div class="imgtext">
+
+						<p>It is interesting to note that ancient India was so advanced in science and mathematics that Algebra, trigonometry and calculus all came from India. In the 11th century Sridharacharya propounded the Quadratic equations. </p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+				<div class="ex3" id="exfixed">
+
+					<img src="images/ex3i.jpg"/>
+
+					<div class="imgtitle">
+
+						<h4> Medicine </h4>
+
+					</div>
+
+					<div class="imgtext">
+
+						<p>Ayurveda is the earliest school of medicine known to mankind. The Father of Medicine, Charaka, consolidated Ayurveda 2500 years ago. </p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+				<div class="ex4" id="exfixed">
+
+					<img src="images/ex4i.jpg"/>
+
+					<div class="imgtitle">
+
+						<h4> Railways </h4>
+
+					</div>
+
+					<div class="imgtext">
+
+						<p>The total distance covered by the 14,300 trains on the Indian Railways everyday, equals three & half times the distance to moon</p>
+
+					</div>
+
+					<div class="imglink">
+
+						<a href="#">Learn More (PPC)</a>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<a id="vote"></a><div class="greyrow" id="advertwhite">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<p><a href="adverts/ads.html">Advertise Here!</a></p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="albumrow">
+
+<div class="stylelayer" id="stylelayervote">
+
+	<div class="headconbody">
+
+		<div class="row">
+
+			<div class="description">
+
+				<h6> VOTE TO WIN! </h6>
+				<p id="albump"> Here it is! This is the ultimate stand off between these two BRIC countries! Vote now!</p></br>
+
+					<form action="index.php" method="post" style="padding: 40px; background-color: rgba(0, 0, 0, 0.4);">
+
+					<p id="formp">Choose one of the two countries and click on vote!</p></br>
+
+					<div class="ch" id="vl"><input type="radio" name="formChina"> China</div>
+					<div class="in" id="vl"><input type="radio" name="formIndia"> India</div></br></br>
+
+					<input type="submit" name="submitbut" value="Vote!">
+
+					</form>
+
+			</div>
+
+		</div>
+	
+	</div>
+
+</div>
+
+</div>
+
+<div class="greyrow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<p><a href="adverts/ads.html">Advertise Here!</a></p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="whiterow">
+
+	<div class="headconbody">
+
+		<div class="row">
+			
+			<div class="description">
+
+				<div class="footer">
+
+				<p>The voting will stop every month and thus, the country with the most votes wins!</br> It will be honoured in a rankinglist! So be sure to vote and share! </br>We will then vote between other countries!</p>
+				</br>
+				<p>Support us, keep the website online! Click on our sponsors below!</p>
+				</br>
+				<a href="adverts/ads.html">Advertise Here! [TEXT ADS]</a>
+				</br>
+				<a href="#">Privacy Policy |</a><a href="#"> Terms and Conditions |</a><a href="#"> Hoster</a>
+
+				</div>			
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+</body>
+
+</html>
