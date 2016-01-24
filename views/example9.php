0a25157fa8d2f305cc07c97ae96b77616c6597c5
Amir Zaidi <azaidi@live.nl>
2016-01-24 16:07:06 +0100

    new CSS example

---
+++ views/example9.php
@@ -0,0 +1,12 @@
+<?php
+$css[] = 'https://fonts.googleapis.com/css?family=Roboto:400,500,700,300';
+?>
+
+<style type="text/css">
+body {
+	font-family: Roboto;
+	font-size: 50px;
+}
+</style>
+
+This is Roboto, from Google CSS!
