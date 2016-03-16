bc1868fc1dac48db7d5474ef856266f586221b77
Amir Zaidi <azaidi@live.nl>
2016-03-16 11:29:38 +0100

    new design added

---
+++ themes/dropdown/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<head>
+	<title>Dropdown</title>
+	<link rel="stylesheet" type="text/css" href="main.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=0.85">
+</head>
+<body>
+	<div id="dropdown">
+		<div id="dropdownmenu">
+			<a href="#"><div>Frontpage</div></a>
+			<a href="#1"><div>Link 1</div></a>
+			<a href="#2"><div>Link 2</div></a>
+			<a href="#3"><div>Link 3</div></a>
+			<a href="#4"><div>Link 4</div></a>
+			<a href="#5"><div>Link 5</div></a>
+			<a href="#6"><div>Link 6</div></a>
+			<a href="#7"><div>Link 7</div></a>
+			<a href="#8"><div>Link 8</div></a>
+		</div>
+		<a href="#">
+			<div id="shadow">
+			</div>
+		</a>
+	</div>
+	<div id="content">
+		<div>
+			<a href="#dropdown">Open Navigation</a>
+		</div>
+		<div id="1" style="height: 200px">
+			Text
+		</div>
+		<div id="2" style="height: 175px">
+			Text
+		</div>
+		<div id="3" style="height: 200px">
+			Text
+		</div>
+		<div id="4" style="height: 175px">
+			Text
+		</div>
+		<div id="5" style="height: 200px">
+			Text
+		</div>
+		<div id="6" style="height: 175px">
+			Text
+		</div>
+		<div id="7" style="height: 200px">
+			Text
+		</div>
+		<div id="8" style="height: 175px">
+			Text
+		</div>
+	</div>
+</body>
+</html>
