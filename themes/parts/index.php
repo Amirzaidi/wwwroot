022aa5ccf63f6abe94d3a87ff161e9361d9a72f9
Amir Zaidi <azaidi@live.nl>
2016-03-17 17:39:34 +0100

    nieuwe layout

---
+++ themes/parts/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+	<title>Dropdown</title>
+	<link rel="stylesheet" type="text/css" href="main.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=0.85">
+</head>
+<body id="select1">
+<div id="select2">
+<div id="select3">
+<div id="select4">
+	<div id="part1" class="part">
+		<div>
+			<a href="#select1">1</a> - 
+			<a href="#">Stop</a>
+		</div>
+	</div>
+	<div id="part2" class="part">
+		<div>
+			<a href="#select2">2</a>
+		</div>
+	</div>
+	<div id="part3" class="part">
+		<div>
+			<a href="#select3">3</a>
+		</div>
+	</div>
+	<div id="part4" class="part">
+		<div>
+			<a href="#select4">4</a>
+		</div>
+	</div>
+</div>
+</div>
+</div>
+</body>
+</html>
