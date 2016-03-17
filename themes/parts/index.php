615ff063965d272dfd5a1948966cb0b34950b845
Amir Zaidi <azaidi@live.nl>
2016-03-17 18:54:00 +0100

    Updates

--- themes/parts/index.php
+++ themes/parts/index.php
@@ -10,23 +10,35 @@
 <div id="select4">
 	<div id="part1" class="part">
 		<div>
-			<a href="#select1">1</a> - 
-			<a href="#">Stop</a>
+			<a href="#select1"><div>+</div></a>
+			<div style="min-height: 500px; height: calc(100% - 40px); background: url('http://www.wallpapermade.com/images/wallpapers/originals/bridge-over-the-river-autumn-landscape-wallpaper-5145.jpg') center; background-size: cover">
+				<a href="#">Stop</a>
+			</div>
 		</div>
 	</div>
 	<div id="part2" class="part">
 		<div>
-			<a href="#select2">2</a>
+			<a href="#select2"><div>+</div></a>
+			<div style="height: 1000px">
+				Random Text
+			</div>
 		</div>
 	</div>
 	<div id="part3" class="part">
 		<div>
-			<a href="#select3">3</a>
+			<a href="#select3"><div>+</div></a>
+			<div style="height: 500px">
+				<div style="height: 100px; width: 100%; background: url('http://www.wallpapermade.com/images/wallpapers/originals/bridge-over-the-river-autumn-landscape-wallpaper-5145.jpg') center; background-size: cover"></div>
+				Random Text
+			</div>
 		</div>
 	</div>
 	<div id="part4" class="part">
 		<div>
-			<a href="#select4">4</a>
+			<a href="#select4"><div>+</div></a>
+			<div>
+				Random Text
+			</div>
 		</div>
 	</div>
 </div>
