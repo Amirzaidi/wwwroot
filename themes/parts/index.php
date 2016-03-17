c8acdab164d60fef8cbb349d264f84e64d48e5e6
Amir Zaidi <azaidi@live.nl>
2016-03-17 19:48:47 +0100

    Fixes and close button

--- themes/parts/index.php
+++ themes/parts/index.php
@@ -8,34 +8,68 @@
 <div id="select2">
 <div id="select3">
 <div id="select4">
+	<a href="#" id="close" class="button"><div>X</div></a>
 	<div id="part1" class="part">
 		<div>
-			<a href="#select1"><div>+</div></a>
-			<div style="min-height: 500px; height: calc(100% - 40px); background: url('http://www.wallpapermade.com/images/wallpapers/originals/bridge-over-the-river-autumn-landscape-wallpaper-5145.jpg') center; background-size: cover">
-				<a href="#">Stop</a>
+			<a href="#select1" class="button"><div>+</div></a>
+			<div style="min-height: calc(100% - 40px); background: url('http://www.wallpapermade.com/images/wallpapers/originals/bridge-over-the-river-autumn-landscape-wallpaper-5145.jpg') center; background-attachment: local; background-size: cover">
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
 			</div>
 		</div>
 	</div>
 	<div id="part2" class="part">
 		<div>
-			<a href="#select2"><div>+</div></a>
-			<div style="height: 1000px">
-				Random Text
+			<a href="#select2" class="button"><div>+</div></a>
+			<div>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
 			</div>
 		</div>
 	</div>
 	<div id="part3" class="part">
 		<div>
-			<a href="#select3"><div>+</div></a>
+			<a href="#select3" class="button"><div>+</div></a>
 			<div style="height: 500px">
 				<div style="height: 100px; width: 100%; background: url('http://www.wallpapermade.com/images/wallpapers/originals/bridge-over-the-river-autumn-landscape-wallpaper-5145.jpg') center; background-size: cover"></div>
-				Random Text
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
+				Random Text<br/>
 			</div>
 		</div>
 	</div>
 	<div id="part4" class="part">
 		<div>
-			<a href="#select4"><div>+</div></a>
+			<a href="#select4" class="button"><div>+</div></a>
 			<div>
 				Random Text
 			</div>
