bcb3f2c503299991d43f3d6a614e4bf0aeb21622
Achie Fa <achie500@hotmail.com>
2016-06-13 23:35:48 +0200

    medals optimalization
    have questions;
    needs some work

--- views/medals.php
+++ views/medals.php
@@ -21,6 +21,7 @@ $language->load('index');
 </div>
 
 <a id="leaderboard">
+
 <div class="whiterow">
 
 	<div class="headconbody">
@@ -30,127 +31,194 @@ $language->load('index');
 			<div class="description">
 
 				<h3>Leaderboard</h3>
-				<p>Here you will see the results of every voting contest! They will be put in archive at the end of the year</p>
+				<p>Here you will be informed about the amount of medals each countries has won!</p>
 
 				<div class="contentrow">
 
 					<div class="col-md-4">
 
-						<div class="contestinfo">
+						<div class="conx">
+							<h1><?php $country = new country(1); echo $country->name; ?> </h1>
 
-							<h1>India <img src="/style/images/vs.png"/> U.S. America</h1>
-								<div class="datecontest">
-									<p>20-12-2015 to 4-1-2016</p>
-								</div>
+							<div class="medals">
+								<p><?php $country = new country(1); 
+									echo $country->medal()->count(); ?> medals in total</p>
 
-								<div class="votingwin">
-									<p><img src="/style/images/winvoting.png"/>India</p>
-								</div>
+								<p id="topmdld">Of Which</p>
 
+							<div class="medaldetails">
+								<p><?php
+									$country = new country(1);
+									$medal = $country->medal();
+										while ($medal->found())
+										{
+											echo '<img src="/style/images/medal_', $medal->type, '.png" />';
+										}
+								?></p>
+							</div>
+
+							</div>
 						</div>
+					
+					</div>
 
-						<div class="contestinfo">
+					<div class="col-md-4">
+						
+						<div class="conx">
+							<h1><?php $country = new country(2); echo $country->name; ?> </h1>
+						</div>
+						
+					</div>
 
-							<h1>China <img src="/style/images/vs.png"/> India</h1>
-								<div class="datecontest">
-									<p id="timer">13d 23:59:34 till end</p>
-								</div>
+					<div class="col-md-4">
 
-								<div class="votingwin">
-									<p><img src="/style/images/winvoting.png"/></p>
-								</div>
-
+						<div class="conx">
+							<h1><?php $country = new country(3); echo $country->name; ?> </h1>
 						</div>
 
 					</div>
 
 					<div class="col-md-4">
 						
-						<div class="contestinfo">
+						<div class="conx">
+							<h1><?php $country = new country(4); echo $country->name; ?> </h1>
+						</div>
+						
+					</div>
 
-							<h1>India <img src="/style/images/vs.png"/> U.S. America</h1>
-								<div class="datecontest">
-									<p>20-12-2015 to 4-1-2016</p>
-								</div>
+					<div class="col-md-4">
 
-								<div class="votingwin">
-									<p><img src="/style/images/winvoting.png"/>India</p>
-								</div>
-
+						<div class="conx">
+							<h1><?php $country = new country(5); echo $country->name; ?> </h1>
 						</div>
 
-						<div class="contestinfo">
+					</div>
 
-							<h1>China <img src="/style/images/vs.png"/> India</h1>
-								<div class="datecontest">
-									<p id="timer">13d 23:59:34 till end</p>
-								</div>
+					<div class="col-md-4">
+						
+						<div class="conx">
+							<h1><?php $country = new country(6); echo $country->name; ?> </h1>
+						</div>
+						
+					</div>
 
-								<div class="votingwin">
-									<p><img src="/style/images/winvoting.png"/></p>
-								</div>
+					<div class="col-md-4">
 
+						<div class="conx">
+							<h1><?php $country = new country(7); echo $country->name; ?> </h1>
 						</div>
 
 					</div>
 
 					<div class="col-md-4">
 
-						<div class="contestinfo">
-
-							<h1>India <img src="/style/images/vs.png"/> U.S. America</h1>
-								<div class="datecontest">
-									<p>20-12-2015 to 4-1-2016</p>
-								</div>
-
-								<div class="votingwin">
-									<p><img src="/style/images/winvoting.png"/>India</p>
-								</div>
-
-						</div>
-
-						<div class="contestinfo">
-
-							<h1>China <img src="/style/images/vs.png"/> India</h1>
-								<div class="datecontest">
-									<p id="timer">13d 23:59:34 till end</p>
-								</div>
-
-								<div class="votingwin">
-									<p><img src="/style/images/winvoting.png"/></p>
-								</div>
-
+						<div class="conx">
+							<h1><?php $country = new country(8); echo $country->name; ?> </h1>
 						</div>
 
 					</div>
 
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(9); echo $country->name; ?> </h1>
+						</div>
+					
+					</div>
+
+					<div class="col-md-4">
+						
+						<div class="conx">
+							<h1><?php $country = new country(10); echo $country->name; ?> </h1>
+						</div>
+						
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(11); echo $country->name; ?> </h1>
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+						
+						<div class="conx">
+							<h1><?php $country = new country(12); echo $country->name; ?> </h1>
+						</div>
+						
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(13); echo $country->name; ?> </h1>
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+						
+						<div class="conx">
+							<h1><?php $country = new country(14); echo $country->name; ?> </h1>
+						</div>
+						
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(15); echo $country->name; ?> </h1>
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(16); echo $country->name; ?> </h1>
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(17); echo $country->name; ?> </h1>
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+						
+						<div class="conx">
+							<h1><?php $country = new country(18); echo $country->name; ?> </h1>
+						</div>
+						
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(19); echo $country->name; ?> </h1>
+						</div>
+
+					</div>
+
+					<div class="col-md-4">
+
+						<div class="conx">
+							<h1><?php $country = new country(20); echo $country->name; ?> </h1>
+						</div>
+
+					</div>
+
+
+
 					<div class="col-md-12">
 
 						<h3> Caption </h3>
-
-						<div class="contentrow">
-
-							<div class="col-md-24">
-								<h1>Versus</h1><img src="/style/images/vs.png"></img><p>The Standoff</p>
-							</div>
-
-							<div class="col-md-24">
-								<h1>Medal</h1><img src="/style/images/winvoting.png"></img><p>Won a voting contest</p>
-							</div>
-
-							<div class="col-md-24">
-								<h1>Trophy </br> Rookie </h1><img src="/style/images/trophy.png"></img><p>Won a year of voting</p>
-							</div>
-
-							<div class="col-md-24">
-								<h1>Honour </br> Veteran </h1><img src="/style/images/honour.png"></img><p>Won two years of voting</p>
-							</div>
-
-							<div class="col-md-24">
-								<h1>Crown </br> Emperor of cso.com</h1><img src="/style/images/win4years.png"></img><p>Won four years of voting</p>
-							</div>
-
-						</div>
 
 						<div class="contentrow">
 
