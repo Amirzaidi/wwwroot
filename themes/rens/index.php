c24ce433974f1f9421be61042219fa37e899e7c7
Amir Zaidi <azaidi@live.nl>
2016-06-13 20:27:19 +0200

    Rens concept theme added

---
+++ themes/rens/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<head>
+	<title>Rens MD</title>
+	<link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+<body id="draw">
+	<div id="navbar">
+		<div>
+			<a href="#draw">Menu</a>
+		</div>
+		</div>
+	</div>
+	<div id="header">
+		<div id="button">
+		</div>
+	</div>
+	<div id="content">
+		<div id="nav">
+		</div>
+		<div id="cards">
+			<div>kekz<br/><br/>kekz<br/><br/>kekz<br/><br/>kekz<br/><br/>
+			</div>
+			
+			<div>kekz<br/><br/>kekz<br/><br/>kekz<br/><br/>kekz<br/><br/>
+			</div>
+
+			<div>kekz<br/><br/>kekz<br/><br/>kekz<br/><br/>kekz<br/><br/>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
