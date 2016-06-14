da353f023e056198efb03022a27478bc0ac5eae6
Amir Zaidi <azaidi@live.nl>
2016-06-14 18:51:59 +0200

    Language fix

--- views/inc.footer.php
+++ views/inc.footer.php
@@ -13,7 +13,7 @@
 </div>
 <div id="google_translate_element" style="position: fixed; bottom: 0; right: 0;"></div><script type="text/javascript">
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.BOTTOM_RIGHT, multilanguagePage: true}, 'google_translate_element');
+  new google.translate.TranslateElement({pageLanguage: '<?php echo $session->language; ?>', layout: google.translate.TranslateElement.FloatPosition.BOTTOM_RIGHT, autoDisplay: true, multilanguagePage: true}, 'google_translate_element');
 }
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-<style type="text/css">body { top: 0 !important; } .skiptranslate { display: none !important; }</style>
+<style type="text/css">body { top: 0 !important; } div.skiptranslate > iframe { display: none !important; }</style>
