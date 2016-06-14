89b6490af55dd81f5d9928d203162472311c8b12
Achie Fa <achie500@hotmail.com>
2016-06-15 00:54:14 +0200

    error page style added

--- views/404.php
+++ views/404.php
@@ -1,1 +1,16 @@
-here comes dat boi
+
+
+<div class="errorheader">
+	<div class="errorcontainer">
+		<div class="errorimg">
+			<img src="/style/images/404.gif">
+		</div>
+
+		<div class="errortxt">
+			<h1 id="errtxt" style="font-size: 55px; color: #000;"> 404 error! </h1>
+			<p id="errtxt"> Don't panic! We just didn't understand you :( </p>
+			<p id="errtxt"> The page you requested is not in this universe... </p>
+			<p id="errtxt"><a href="<?php echo '$session->lastpage'; ?>">Previous page</a> | <a href="../index">Home page</a></p>
+		</div>
+	</div>
+</div>
