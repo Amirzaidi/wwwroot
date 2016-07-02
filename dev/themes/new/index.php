c046787f6f7e2664d361307a6f374c327f3627da
Amir Zaidi <azaidi@live.nl>
2016-07-02 02:03:20 +0200

    Implemented Ahmad's checkbox trick for read more button

--- dev/themes/new/index.php
+++ dev/themes/new/index.php
@@ -1,55 +1,72 @@
 <!DOCTYPE html>
 <head>
-	<title>Accuracy</title>
+	<title>New Theme</title>
 	<link rel="stylesheet" type="text/css" href="main.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="theme-color" content="#ff5722" />
 </head>
 <body>
 	<div id="navbar" class="d2 orange">
-		<input type="checkbox" id="navbox" class="hide" />
-		<label for="navbox" id="navboxlabel" style="color: white"><div>=</div></label>
+		<a href="#open" id="open"><div>=</div></label>
+		<a href="#" id="close"><div></div></a>
 
-		<div id="nav" class="d5">
-			<a style="color: white"><div class="orange">New Theme</div></a>
-			<?php for ($i = 6; $i < 16; $i++) { ?>
-			<a href="/dev/themes/new/index.php"><div class="hd1">Kek</div></a>
+		<div id="nav">
+			<a style="color: white" href="#"><div class="orange" style="padding-top: 152px;">New Theme</div></a>
+			<a href="/dev/themes/new/index.php#navbar"><div>Open Navbar</div></a>
+			<a id="current"><div>Selected Page</div></a>
+			<?php for ($i = 0; $i < 5; $i++) { ?>
+			<a href="/dev/themes/new/index.php"><div>Navigation Page</div></a>
 			<?php } ?>
-		</div>
-
-		<div id="search">
-			<form action="#" method="post">
-				<input type="text" placeholder="Search..." name="search" />
-				<input type="submit" value="Q" />
-			</form>
 		</div>
 	</div>
 
 	<div id="content">
 		<div id="header">
+			<div><p>This is a topkek top header</p></div>
 		</div>
 
-		<div>
-		<?php for ($i = 6; $i < 16; $i++) { ?>
-		<div class="d1 hd2"><?php for ($j = 2; $j < $i; $j++) echo 'AYY<br/>'; ?>
-		</div>
-		<?php } ?>
-		</div>
+		<div id="columns">
+			<input type="checkbox" id="e1" class="hide" />
+			<div id="e1">
+				<div class="photo"><img src="http://i.imgur.com/9iWpm5z.jpg" /></div>
+				<div class="text"><b>Info</b><br/>Subinfo</div>
+				<img src="http://i.imgur.com/9iWpm5z.jpg" />
+				<p><?php for ($j = 2; $j < 5; $j++) echo 'AYY<br/>'; ?></p>
+				<div class="collapse">
+					<p><a href="#">Download</a><br/><a href="#">Share</a></p>
+				</div>
+				<label for="e1"><p>VIEW MORE <b style="float: right">&#x25BE;</b></p></label>
+			</div>
 
-		<div>
-		<?php for ($i = 5; $i < 14; $i++) { ?>
-		<div class="d1 hd2"><?php for ($j = 0; $j < $i; $j++) echo 'LMAO<br/>'; ?>
-		</div>
-		<?php } ?>
+			<?php for ($i = 2; $i <= 10; $i++) { ?>
+			<input type="checkbox" id="e<?php echo $i; ?>" class="hide" />
+			<div id="e<?php echo $i; ?>">
+				<p><?php for ($j = 0; $j < $i; $j++) echo 'AYY<br/>'; ?></p>
+				<div class="collapse">
+					<p><a href="#">Download</a><br/><a href="#">Share</a></p>
+				</div>
+				<label for="e<?php echo $i; ?>"><p>VIEW MORE <b style="float: right">&#x25BE;</b></p></label>
+			</div>
+			<?php } ?>
 		</div>
 
 		<div id="footer">
-			<div class="d1 hd2">Kek</div>
+			<div><p>Kek</p></div>
 		</div>
 	</div>
 
+	<style type="text/css">
+	<?php
+	for ($i = 1; $i <= 10; $i++)
+	echo 'input#e', $i, ':checked ~ div#e', $i, ' > div.collapse { max-height: 100px }
+	input#e', $i, ':checked ~ div#e', $i, ' > label { max-height: 0 }
+	';
+	?>
+	</style>
+
 	<input type="checkbox" id="toastbox" class="hide" />
 	<div id="toast">
-		<div class="d2 hd4"><label for="toastbox" id="toastboxlabel">&#215;</label> You were logged in as Kyou-kun</div>
+		<div class="d3 hd5"><label for="toastbox" id="toastboxlabel">&#215;</label> You were logged in as Kyou-kun</div>
 	</div>
 </body>
 </html>
